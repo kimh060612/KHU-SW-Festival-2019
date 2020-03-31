0e94c08b1d09887f67da82bc212351910edd5121
Kim <kimh060612@naver.com>
2020-04-01 04:01:20 +0900

    .

---
+++ Source_code/In-game data analysis/DeepRegressor.py
@@ -0,0 +1,18 @@
+import keras.backend as K
+from keras.layers import Dense
+from keras.optimizers import adam
+from keras.layers import BatchNormalization
+
+class DeepRegression:
+    def __init__(self):
+        
+        pass
+    
+    def Regression_model(self):
+        pass
+
+    def TrainModel(self):
+        pass
+
+    def TestModel(self):
+        pass

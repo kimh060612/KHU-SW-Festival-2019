d4475e2eb341bd94ed0297ad571475e803cb74be
kimh060612 <kimh060612@naver.com>
2020-04-13 19:48:58 +0900

    .

--- Source_code/In-game_data_analysis/DeepRegressor.py
+++ Source_code/In-game_data_analysis/DeepRegressor.py
@@ -6,9 +6,23 @@ from keras.layers import Dropout
 from keras.layers import BatchNormalization
 from keras.models import Sequential
 from keras.optimizers import adam
+import tensorflow as tf
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import os
+
+def get_session(gpu_fraction=0.4):
+    '''Assume that you have 6GB of GPU memory and want to allocate ~2GB'''
+
+    num_threads = os.environ.get('OMP_NUM_THREADS')
+    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=gpu_fraction)
+
+    if num_threads:
+        return tf.Session(config=tf.ConfigProto(
+            gpu_options=gpu_options, intra_op_parallelism_threads=num_threads))
+    else:
+        return tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
 
 class PrintDot(Callback):
     def on_epoch_end(self, epoch, logs):
@@ -28,19 +42,8 @@ def plot_history(history):
             label='Train Error')
     plt.plot(hist['epoch'], hist['val_mae'],
             label = 'Val Error')
-    plt.ylim([0,5])
+    plt.ylim([0,1])
     plt.legend()
-
-    plt.subplot(2,1,2)
-    plt.xlabel('Epoch')
-    plt.ylabel('Mean Square Error [$MPG^2$]')
-    plt.plot(hist['epoch'], hist['mse'],
-            label='Train Error')
-    plt.plot(hist['epoch'], hist['val_mse'],
-            label = 'Val Error')
-    plt.ylim([0,20])
-    plt.legend()
-    plt.show()
 
     
 
@@ -56,16 +59,17 @@ class DeepRegression:
         Adam = adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999)
         
         model = Sequential()
-        model.add(Dense(50, input_dim=self.Shape, activation="relu"))
-        model.add(Dense(100, activation="relu"))
+        model.add(Dense(128, input_dim=self.Shape, activation="relu"))
+        model.add(Dense(512, activation="relu"))
         model.add(BatchNormalization(momentum=0.99, epsilon=0.001))
-        model.add(Dense(100, activation="relu"))
+        model.add(Dense(512, activation="relu"))
         model.add(Dropout(0.5))
         model.add(BatchNormalization(momentum=0.99, epsilon=0.001))
-        model.add(Dense(100, activation="relu"))
+        model.add(Dense(512, activation="relu"))
         model.add(Dropout(0.5))
         model.add(BatchNormalization(momentum=0.99, epsilon=0.001))
-        model.add(Dense(10, activation="relu"))
+        model.add(Dense(128, activation="relu"))
+        model.add(Dropout(0.5))
         model.add(Dense(1, activation="linear"))
         model.compile(loss="mean_squared_error", optimizer = Adam, metrics = ["mse","mae"])
 
@@ -75,7 +79,8 @@ class DeepRegression:
         
 
     def Train(self, epoch):
-
+        
+        K.set_session(get_session())
         early_stop = EarlyStopping(monitor='val_loss', patience=10)
         History = self.Regressor.fit(self.Data, self.Pred, batch_size=32, epochs=epoch,validation_split = 0.2, verbose=0, callbacks=[early_stop, PrintDot()])
 
--- Source_code/In-game_data_analysis/REGRESSION_CLASS_RE.py
+++ Source_code/In-game_data_analysis/REGRESSION_CLASS_RE.py
@@ -358,7 +358,7 @@ class REGRESSION:
             for index in dependence_plot_name :
                 shap.dependence_plot(index, shap_val, DATA_TABLE)
         elif REGRESSOR_NAME == "Deep":
-            explainer = shap.DeepExplainer(self.DeepREG, DATA_TABLE)
+            explainer = shap.DeepExplainer(self.DeepREG.Regressor, DATA_TABLE)
             shap_val = explainer.shap_values(DATA_TABLE)
             shap.summary_plot(shap_val, DATA_TABLE, plot_type="bar",max_display=21)
             shap.summary_plot(shap_val,DATA_TABLE, plot_type="dot",max_display=21)
